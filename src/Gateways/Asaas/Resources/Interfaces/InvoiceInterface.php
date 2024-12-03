5d71f78637a8ea4b96d7bfd5713c766e0622445b
Mário Lucas <mariolucasdev@gmail.com>
2024-12-03 11:57:30 -0300

    PAYH-20: feat: get qrcode pix from invoice

--- src/Gateways/Asaas/Resources/Interfaces/InvoiceInterface.php
+++ src/Gateways/Asaas/Resources/Interfaces/InvoiceInterface.php
@@ -6,5 +6,11 @@ use PHPay\Exceptions\AsaasExceptions;
 
 interface InvoiceInterface
 {
+    // public function all(): self;
+    // public function get(): array;
+    // public function with(array $filters): self;
+    // public function find(string $invoiceId): array;
     public function create(): self|AsaasExceptions;
+    // public function delete(): bool;
+    public function qrCodePix(): array;
 }
