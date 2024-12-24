2a4768c049e4900e019737557022f101b8b38998
evandrosystems <evandrosystems@gmail.com>
2024-12-24 09:35:39 -0300

    PHPAY-29: PHPAY-29: feat: adding get all efi charges

--- examples/efi/charges.php
+++ examples/efi/charges.php
@@ -28,3 +28,18 @@ $phpay
     ->charge($charge)
     ->setCustomer($customer)
     ->create();
+
+
+/**
+ * get all charges
+ * @return array charges
+ */
+$phpay
+    ->charge()
+    ->setQueryParams([
+        'charge_type' => 'billet',
+        'begin_date' => '2024-11-01',
+        'end_date' => '2024-11-30',
+        'status' => 'unpaid'
+    ])
+    ->getAll();
